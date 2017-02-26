89a71f87af5e412323ca377c7819a77abca757d9
karthikeyan <karthisiva66@gmail.com>
2017-02-26 19:57:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,11 +90,15 @@ var htmlTemplate = `
        return htmlTemplate;
 }
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter +1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
@@ -106,6 +110,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
